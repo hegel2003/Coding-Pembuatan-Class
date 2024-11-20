94a7840aca7642f7ef0e15e3bdc1cacfb336f8ed
hegel2003 <culprit774@gmail.com>
2024-11-20 18:41:15 +0700

    Update Makanan.java

--- Makanan.java
+++ Makanan.java
@@ -1,11 +1,28 @@
 package src;
 
-public class Makanan {
-    public String nama;
-    public int harga;
+
+class Makanan {
+    private String nama;
+    private int harga;
 
     public Makanan(String nama, int harga) {
         this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
         this.harga = harga;
     }
 }
