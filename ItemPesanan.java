d3ff19fb8381ee84158af415113d1b2b7b1420d8
hegel2003 <culprit774@gmail.com>
2024-11-20 12:24:25 +0700

    Update ItemPesanan.java

--- ItemPesanan.java
+++ ItemPesanan.java
@@ -1,14 +1,38 @@
 package src;
 
 
-public class ItemPesanan {
-    public Makanan makanan;
-    public int jmlPembelian;
-    public Voucher voucher;
+class ItemPesanan {
+    private Makanan makanan;
+    private int jmlPembelian;
+    private Voucher voucher;
 
     public ItemPesanan(Makanan makanan, int jmlPembelian, Voucher voucher) {
         this.makanan = makanan;
         this.jmlPembelian = jmlPembelian;
+        this.voucher = voucher;
+    }
+
+    public Makanan getMakanan() {
+        return makanan;
+    }
+
+    public void setMakanan(Makanan makanan) {
+        this.makanan = makanan;
+    }
+
+    public int getJmlPembelian() {
+        return jmlPembelian;
+    }
+
+    public void setJmlPembelian(int jmlPembelian) {
+        this.jmlPembelian = jmlPembelian;
+    }
+
+    public Voucher getVoucher() {
+        return voucher;
+    }
+
+    public void setVoucher(Voucher voucher) {
         this.voucher = voucher;
     }
 }
