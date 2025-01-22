e18cfd511e414e9f4ea31140ac9ff86fd6704537
DWI AGI RAHMATULLAH <culprit774@gmail.com>
2025-01-22 09:17:32 +0700

    Update Makanan.java

--- Makanan.java
+++ Makanan.java
@@ -1,13 +1,12 @@
-package src;
-
-
 class Makanan {
     private String nama;
     private int harga;
+    private String Restoran;
 
-    public Makanan(String nama, int harga) {
+    public Makanan(String nama, int harga, String Restoran) {
         this.nama = nama;
         this.harga = harga;
+        this.Restoran = Restoran;
     }
 
     public String getNama() {
@@ -24,5 +23,13 @@ class Makanan {
 
     public void setHarga(int harga) {
         this.harga = harga;
+    }
+
+    public String getRestoran(){
+        return Restoran;
+    }
+
+    public void setRestoran(String Restoran){
+        this.Restoran = Restoran;
     }
 }
